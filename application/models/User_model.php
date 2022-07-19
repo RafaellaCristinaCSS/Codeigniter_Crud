bcb9ba7bb231275a7d0b3c112a4ce4085e2d0bd7
Rafaella Cristina <rafaella.cristina@datte.com.br>
2022-07-19 16:23:20 -0300

    searching the user list

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -3,5 +3,9 @@ class User_model extends CI_Model{
     function create($formArray){
         $this->db->insert('tb_user', $formArray); /* INSERT INTO users (name, email) values (?,?) */
     }
+
+    function all(){
+       return $users = $this->db->get('tb_user')->result_array(); /* Select * From tb_user */
+    }
 }
 ?>
