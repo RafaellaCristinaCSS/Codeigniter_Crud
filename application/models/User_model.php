9b5de426f0016524e4895813caa1d8b3183a58ce
Rafaella Cristina De Sousa Sacramento <69471056+RafaellaCristinaCSS@users.noreply.github.com>
2022-07-20 14:39:34 -0300

    adding function to delete

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -9,11 +9,15 @@ class User_model extends CI_Model{
     }
     function getUser($user_id){
         $this->db->where('use_id', $user_id);
-        return $user = $this->db->get('tb_user')->row_array(); /* Select * From users where user_id = ? */
+        return $user = $this->db->get('tb_user')->row_array(); /* Select * From users Where user_id = ? */
     }
     function updateUser($user_id, $formArray){
         $this->db->where('use_id',$user_id);
-        $this->db->update('tb_user',$formArray); /* Update users SET name = ? , email = ? where user_id = ?  */
+        $this->db->update('tb_user',$formArray); /* Update users SET name = ? , email = ? Where user_id = ?  */
+    }
+    function deleteUser($user_id){
+        $this->db->where('use_id',$user_id);
+        $this->db->delete('tb_user'); /* Delete From users Where use_id = ? */
     }
 }
 ?>
